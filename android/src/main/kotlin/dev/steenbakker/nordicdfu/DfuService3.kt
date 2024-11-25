5f9189250df9ecd60a11843e480031c63f661a94
Flasher-MS <martin.simpson@flasher.tech>
2024-11-25 21:33:51 +0100

    Update NordicDfuPlugin (Android): handle parallel DFU processes (up to 8); add optional address parameter to abortDfu; add DFU service classes to AndroidManifest.xml

---
+++ android/src/main/kotlin/dev/steenbakker/nordicdfu/DfuService3.kt
@@ -0,0 +1,17 @@
+package dev.steenbakker.nordicdfu
+
+import android.app.Activity
+import no.nordicsemi.android.dfu.DfuBaseService
+
+class DfuService3 : DfuBaseService() {
+    override fun getNotificationTarget(): Class<out Activity?> {
+        return NotificationActivity::class.java
+    }
+
+    override fun isDebug(): Boolean {
+        // Override this method and return true if you need more logs in LogCat
+        // Note: BuildConfig.DEBUG always returns false in library projects, so please use
+        // your app package BuildConfig
+        return true
+    }
+}
